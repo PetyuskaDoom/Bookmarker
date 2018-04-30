7b0b088636f20e20100367c56d7b355974df2c2b
PetyuskaDoom <patzer1941@gmail.com>
2018-04-30 17:25:49 +0100

    Update 4

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log("... main.js linked ...");
+console.log("... main.js linked OK ...");
+
+// Listen for form submit
+document.getElementById('myBtn').addEventListener('submit', saveBookmark);
+
+// Save Bookmark
+function saveBookmark(e){
+  console.log("In saveBookmark");
+
+  e.preventDefault();
+}
+
